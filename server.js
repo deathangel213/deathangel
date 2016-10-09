999cd2765bcac2719cf3dcce6dca50af7d9a7f92
Edward Chen <etchen@gmail.com>
2016-10-09 15:23:19 -0700

    Update dialog

--- server.js
+++ server.js
@@ -1,32 +1,22 @@
 var builder = require('botbuilder');
-var restify = require('restify');
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-  console.log('%s listening to %s', server.name, server.url); 
-});
-
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-
+var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
 
 bot.dialog('/', [
    function (session) {
-        builder.Prompts.text(session, "I’m practicing metal right now, but I always have time for my fellow LA metalheads!:the_horns: What’s your name?");
+        builder.Prompts.text(session, " I’m practicing metal right now, but I always have time for my fellow LA metalheads!:the_horns: What’s your name?");
     },
     function (session, results) {
         session.userData.name = results.response;
-        builder.Prompts.text(session, results.response + "Metal punched a tin can, hahaha! What's Up?"); 
+        builder.Prompts.number(session, results.response + " Metal punched a tin can, hahaha! What's Up?"); 
     },
     function (session, results) {
-        builder.Prompts.text(session, "My favorite place is Masa in Echo Park");
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "My favorite place is Masa in Echo Park"]);
     },
     function (session, results) {
-        session.send("Well, I’m always metaling…Check out this place ");
+        session.userData.language = results.response.entity;
+        session.send("Well, I’m always metaling…Check out this place "
     }
 ]);
