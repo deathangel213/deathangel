9d33714024c172ae8de54206e5dfa447ddb88a1f
Edward Chen <etchen@gmail.com>
2016-10-10 15:22:00 -0700

    Fix URL for Luis

--- testBot2.js
+++ testBot2.js
@@ -1,13 +1,10 @@
 function buildBot(builder, connector) {
     var bot = new builder.UniversalBot(connector);
 
-
-    var model = 'https://api.projectoxford.ai/luis/v1/application/preview?id=bb83b31a-f451-4b7c-b962-df4d4af75a2c&subscription-key=71cd924cf4d04f82ba6c2b1fd1585530';
+    var model = 'https://api.projectoxford.ai/luis/v1/application?id=bb83b31a-f451-4b7c-b962-df4d4af75a2c&subscription-key=71cd924cf4d04f82ba6c2b1fd1585530';
     var recognizer = new builder.LuisRecognizer(model);
     var intents = new builder.IntentDialog({ recognizers: [recognizer] });
     bot.dialog('/', intents);
-
-
 
     intents.matchesAny(['FindFood', 'findFood', 'findfood', 'deathangel.FindFood', 'deathangel:FindFood'], [
         function (session, args, next) {
